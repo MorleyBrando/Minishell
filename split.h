095a0017776298c0faa8dfce6694bf139075ebcd
Brando Morley <143522146+MorleyBrando@users.noreply.github.com>
2023-10-29 21:59:36 -0400

    Update split.h

--- split.h
+++ split.h
@@ -1,13 +1,6 @@
 #if !defined(SPLIT_H)
 #define SPLIT_H
 
-/* (c) Larry Herman, 20233.  You are allowed to use this code yourself, but
- * not to provide it to anyone else.
- */
-
-/* DO NOT MODIFY THIS FILE OR YOUR CODE WILL NOT COMPILE ON THE SUBMIT
- * SERVER. */
-
 char **split(const char line[]);
 
 #endif
